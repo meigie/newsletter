0a87721a79380546117b7e8d68149db205748421
meigie <meigie.mq@gmail.com>
2025-07-26 22:23:14 +0800

    added context and loading state

--- components/input/newsletter-registration.js
+++ components/input/newsletter-registration.js
@@ -1,18 +1,22 @@
-import { useRef } from 'react';
+import { useRef, useContext } from 'react';
 
 import classes from './newsletter-registration.module.css';
+import NotificationContext from '@/store/notification-context';
 
 function NewsletterRegistration() {
   const emailInputRef = useRef();
+  const notificationCtx = useContext(NotificationContext);
 
   function registrationHandler(event) {
     event.preventDefault();
 
-    // fetch user input (state or refs)
-    // optional: validate input
-    // send valid data to API
-
     const enteredEmail = emailInputRef.current.value;
+
+    notificationCtx.showNotification({
+      title: 'Signing up...',
+      message: 'Registering for newsletter...',
+      status: 'pending',
+    });
 
     fetch('/api/newsletter', {
       method: 'POST',
@@ -23,9 +27,28 @@ function NewsletterRegistration() {
         'Content-Type': 'application/json',
       },
     })
-      .then((response) => response.json())
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+
+        return response.json().then((data) => {
+          throw new Error(data.message || 'Something went wrong!');
+        });
+      })
       .then((data) => {
-        console.log(data);
+        notificationCtx.showNotification({
+          title: 'Success!',
+          message: 'Successfully registered for newsletter!',
+          status: 'success',
+        });
+      })
+      .catch((error) => {
+        notificationCtx.showNotification({
+          title: 'Error!',
+          message: error.message || 'Failed to register for newsletter.',
+          status: 'error',
+        });
       });
   }
 
